2822525cd17854fb11216c3d1fca0c5821bbb82c
Kilan Larsen <kilanlarsen@gmail.com>
2021-03-25 02:32:48 -0700

    .gitignore set up to ignore .vs an package stuff

---
+++ Shapeshift Dungeon/Assets/Scripts/Feedback/AnalyticsEvents.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class AnalyticsEvents : MonoBehaviour
+{
+    //Sends an analytics event when an item is picked up by the player
+    public void ItemTaken(string ItemName)
+    {
+        AnalyticsResult analyticsResult = Analytics.CustomEvent("test");
+    }
+}
