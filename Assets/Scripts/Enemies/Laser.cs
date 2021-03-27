eb5cd0a902f832f78db585cda9b886f5e043dcce
jadonato <jadonato@mail.csuchico.edu>
2021-03-26 19:30:48 -0700

    Some laser stuff setup

--- Assets/Scripts/Enemies/Laser.cs
+++ Assets/Scripts/Enemies/Laser.cs
@@ -12,6 +12,44 @@ public class Laser : MonoBehaviour
      * Date Last Edited: 3/23/2021
      */
 
+    public float legnth = 7f;
+    public float damage = 3f;
+
+    [Header("Misc")]
+    public GameObject laser;
+    public LayerMask mask;
+
+    private LineRenderer beam;
+
+    private void Start()
+    {
+        beam = laser.gameObject.GetComponent<LineRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        SetLaserLength();
+    }
+
+    private void Update()
+    {
+        beam.SetPosition(1, new Vector3(0, 0, legnth));
+    }
+
+    /// <summary>
+    /// Shoots a raycast to determine the length of the laser
+    /// </summary>
+    private void SetLaserLength()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(laser.transform.position, laser.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, mask))
+        {
+            //Debug.Log("Hit " + hit.transform.gameObject.name);
+            legnth = hit.distance;
+        }
+    }
+
     /// <summary>
     /// Sets the scales of all the vfx effects based on a given percentage
     /// </summary>
