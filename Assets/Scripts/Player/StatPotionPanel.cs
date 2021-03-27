1f91db80420f17c4ca376afb32add9fd632c7a30
Alexander H. Lopez <alopez165@mail.csuchico.edu>
2021-03-26 22:56:07 -0700

    Stat potion Math. SO MUCH MATH!

--- Assets/Scripts/Player/StatPotionPanel.cs
+++ Assets/Scripts/Player/StatPotionPanel.cs
@@ -5,7 +5,16 @@ using UnityEngine;
 public class StatPotionPanel : MonoBehaviour
 {
     //Variable to keep track of the percent increase value to be displayed on the UI panel
-    [HideInInspector] public float percentIncrease = 0;
+    [HideInInspector] public float increaseValue = 0;
+
+    private float attackPercentIncrease = 0.15f;
+
+    private float originalAttackBaseValue;
+
+    private void Start()
+    {
+        originalAttackBaseValue = PlayerController.Instance.baseAttackDamage.BaseValue;
+    }
 
     public void IncreaseMaxHealth()
     {
@@ -21,19 +30,35 @@ public class StatPotionPanel : MonoBehaviour
     {
         Debug.Log("Increase Attack");
 
+        float startingValue = PlayerController.Instance.baseAttackDamage.BaseValue;
+
         PlayerController.Instance.StatAttackCount++; //Increases the statAttackCount by 1 as the attack option has been selected
+
+        float MathCalculation = 1f / Mathf.Pow(PlayerController.Instance.StatAttackCount,1/4f);
 
         //Sets the percent increase to the Attack Base value * 15% (with a decreasing percentage by .1) * sqrt times choosen
         //0.151 is the percent as we start at 0 so when we take the first attack increase it will be the desired initial 15%
-        percentIncrease = PlayerController.Instance.baseAttackDamage.BaseValue * (0.151f - (PlayerController.Instance.StatAttackCount / 10)) * (1f / Mathf.Sqrt(PlayerController.Instance.StatAttackCount));
+        float increaseValue = originalAttackBaseValue * attackPercentIncrease * MathCalculation;
 
         //Increases the current base value by the percent increase
-        PlayerController.Instance.baseAttackDamage.BaseValue += percentIncrease;
+        PlayerController.Instance.baseAttackDamage.BaseValue += increaseValue;
 
+        float increasePercent = ((originalAttackBaseValue + increaseValue) / originalAttackBaseValue) - 1;
+
+        //float percentIncrease = (PlayerController.Instance.baseAttackDamage.BaseValue - startingValue); 
 
         HUDController.Instance.HideStatPotionPanel(); //Hides the stat potion panel
 
         AnalyticsEvents.Instance.StatUpgraded("Attack"); //Send Stat Upgraded Analytics Event
+
+        Debug.Log("Starting Value: " + startingValue + "\n" 
+            + "Percent Increase: " + attackPercentIncrease * 100 + "\n" 
+            + "Math Calculation: " + MathCalculation + "\n" 
+            + "Increase Value: " + increaseValue + "\n"
+            + "Final Value: " + PlayerController.Instance.baseAttackDamage.BaseValue + "\n"
+            + "Increase %: " + increasePercent * 100+ "%");
+
+        attackPercentIncrease -= 0.001f;
 
         //(float)Math.Round(finalValue, 1); Rounds the decimal to 1 decimal place
 
