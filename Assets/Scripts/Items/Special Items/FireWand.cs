d8182072d4a99dc54f1424edc0c5381f23ccdb1d
Alexander H. Lopez <alopez165@mail.csuchico.edu>
2021-03-29 16:44:35 -0700

    Fire Wand is implemented and now in testing!

--- Assets/Scripts/Items/Special Items/FireWand.cs
+++ Assets/Scripts/Items/Special Items/FireWand.cs
@@ -11,9 +11,10 @@ public class FireWand : MonoBehaviour
     /// Function to get have the fire ball spawn and begin moving in the direction that the player is facing - AHL (3/29/21)
     /// Takes in the current player position, direction and rotation
     /// </summary>
-    public void spawnBowlingBall(Vector3 playerPos, Vector3 playerDir, Quaternion playerRotation)
+    public void spawnFireBall(Vector3 playerPos, Vector3 playerDir, Quaternion playerRotation)
     {
         Vector3 spawnPos = playerPos + playerDir * 2; //Sets the vector position to spawn the Fireball attack slightly infront of the player
+        spawnPos.y += 1f; //Adjusts the fireball to spawn in the correct position to the player
 
         Instantiate(PlayerFireBall, spawnPos, playerRotation); //Spawns the fireball attack
     }
--- Assets/Scripts/Items/Special Items/FireWandAttack.cs
+++ Assets/Scripts/Items/Special Items/FireWandAttack.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class FireWandAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Variable Initialization/Declaration
+    public float damage; //Damage variable to adjust the damage the enemy takes when it gets hit
+    public float speed; //The speed that the Fire ball will go at
+
+    /// <summary>
+    /// OnTrigger so when it hits an enemy it will damage them - AHL (3/4/21)
+    /// </summary>
+    private void OnTriggerEnter(Collider other)
     {
-        
+        //If the other object is Monster (Contains the enemy base script) then go on with the rest of the damage then destroys itself
+        if (other.GetComponent<EnemyBase>())
+        {
+            other.GetComponent<EnemyBase>().Damage(damage);
+            other.GetComponent<StatusEffects>().fireStatus(5); //Sets the enemy on fire for 5 seconds
+            Destroy(gameObject);
+        }
+
+        else if (other.gameObject.layer == 2 || other.gameObject.layer == 9 || other.gameObject.layer == 10)
+            Destroy(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    //Destroy when colliding with Environment Layer or the Wall Layer
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.layer == 2 || collision.gameObject.layer == 9 || collision.gameObject.layer == 10)
+            Destroy(gameObject);
+    }
+
+    //Update is called once a frame
+    private void Update()
+    {
+        //Every frame the bowling ball will move forward
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 }
