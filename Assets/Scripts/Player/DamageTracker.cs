d9616cfed7cf4649209f0723189bf069cf218567
Alexander H. Lopez <alopez165@mail.csuchico.edu>
2021-03-30 15:06:38 -0700

    Adjustments made for beginning of Damage Tracking for the player

--- Assets/Scripts/Player/DamageTracker.cs
+++ Assets/Scripts/Player/DamageTracker.cs
@@ -5,9 +5,30 @@ using UnityEngine;
 public class DamageTracker : SingletonPattern<DamageTracker>
 {
     //Variable List
-    [HideInInspector] public float spikeDamage = 0, lavaDamage = 0, skullDamage = 0, bombDamage = 0, pitfallDamage = 0; //The different damage types to track the amount of damage taken on the player
-    
+    private float spikeDamage = 0, lavaDamage = 0, skullDamage = 0, crystalDamage = 0, bombDamage = 0, pitfallDamage = 0, fireStatusDamage = 0; //The different damage types to track the amount of damage taken on the player
+
     //Enums
+    /// <summary>
+    /// Choice of what damage source the player took - AHL (3/30/21)
+    /// </summary>
+    public enum damageSourceTypes
+    {
+        SpikeDamageSource = 0,
+        LavaDamageSource = 1,
+        PitfallDamageSource = 2,
+        SkullDamageSource = 3,
+        CrystalDamageSource = 4,
+        BombDamageSource = 5,
+        FireStatusDamageSource = 6,
+    }
+
+    /// <summary>
+    /// Updates the damage float values with the amount of damage that the player took based on the game object source - AHL (3/30/21)
+    /// </summary>
+    public void updateDamage(GameObject damageSource, float damageValue)
+    {
+        //Long chain of ifs to adjust the daamge variables listed above
+    }
 
     /// <summary>
     /// When the player dies then we display the total amount of damage the player has taken - AHL (3/23/21)
@@ -15,10 +36,12 @@ public class DamageTracker : SingletonPattern<DamageTracker>
     public void displayDamage()
     {
         print("These are the damage sources and amount of damage that the player has taken throughout the test:");
-        print("The Amount of Spike damage the player took was: " + spikeDamage);
-        print("The Amount of Lava damage the player took was: " + lavaDamage);
-        print("The Amount of Skull damage the player took was: " + skullDamage);
-        print("The Amount of Bomb damage the player took was: " + bombDamage);
-        print("The Amount of Pit Fall damage the player took was: " + pitfallDamage);
+        print("The Amount of Spike damage the player took was: " + spikeDamage + 
+            "\nThe Amount of Lava damage the player took was: " + lavaDamage +
+            "\nThe Amount of Pit Fall damage the player took was: " + pitfallDamage +
+            "\nThe Amount of Skull damage the player took was: " + skullDamage +
+            "\nThe Amount of Crystal damage the player took was: " + crystalDamage +
+            "\nThe Amount of Bomb damage the player took was: " + bombDamage + 
+            "\nThe Amount of Fire Status damage the player took was: " + fireStatusDamage);
     }
 }
