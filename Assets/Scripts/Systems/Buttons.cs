fab56272ca6c0a9125481fe0d4116cbe44c89d66
Kilan Larsen <kilanlarsen@gmail.com>
2021-04-06 15:46:48 -0700

    Grass Tiles

--- Assets/Scripts/Systems/Buttons.cs
+++ Assets/Scripts/Systems/Buttons.cs
@@ -9,23 +9,43 @@ public class Buttons : MonoBehaviour
 {
     public GameObject buttonsPanel;
     public GameObject[] buttons;
+    public EventSystem eventSystem;
+
+    private int buttonIndex;
 
     private void Start()
     {
         //Clear selected object
-        //EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(null);
+        
         //Set a new selected object
-        //EventSystem.current.SetSelectedGameObject(buttons[0]);
+        buttonIndex = 0;
+        //buttons[buttonIndex].GetComponent<Button>().Select();
+        eventSystem.GetComponent<EventSystem>().SetSelectedGameObject(buttons[buttonIndex]);
     }
 
     public void NextButton()
     {
+        buttonIndex++;
+        if (buttonIndex > buttons.Length - 1)
+            buttonIndex = 0;
 
+        //buttons[buttonIndex].GetComponent<Button>().Select();
+
+        eventSystem.GetComponent<EventSystem>().SetSelectedGameObject(buttons[buttonIndex]);
+        Debug.Log("NextButton Performed");
     }
 
     public void PreviousButton()
     {
+        buttonIndex--;
+        if (buttonIndex < 0)
+            buttonIndex = buttons.Length - 1;
 
+        //buttons[buttonIndex].GetComponent<Button>().Select();
+
+        eventSystem.GetComponent<EventSystem>().SetSelectedGameObject(buttons[buttonIndex]);
+        Debug.Log("PreviousButton Performed");
     }
 
     public void QuitGame()
