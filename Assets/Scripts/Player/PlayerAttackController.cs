f0488c26ebcd27f41a63bbb36fbd20054a744ba2
Kilan Larsen <kilanlarsen@gmail.com>
2021-04-11 21:36:19 -0700

    New Implementation of Player Controller, Bridge tiles now have pits

---
+++ Assets/Scripts/Player/PlayerAttackController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackController : SingletonPattern<PlayerAttackController>
+{
+    [SerializeField] private GameObject slashHitbox; //GameObject to hold slash attack hitbox
+    [SerializeField] private GameObject thrustHitbox; //GameObject to hold thrust attack hitbox
+    [SerializeField] private GameObject radialHitbox; //GameObject to hold radial attack hitbox
+    [SerializeField] private GameObject swordImpactPoint; //GameObject to hold point of impact on third attack
+    [SerializeField] private bool showHitboxes = false;
+
+    //Activate the slash hitbox - called from attack animation event
+    public void ActivateSlashHitbox()
+    {
+        slashHitbox.GetComponent<MeshCollider>().enabled = true;
+
+        if (showHitboxes)
+            slashHitbox.GetComponent<MeshRenderer>().enabled = true;
+    }
+
+    //Activate the thrust hitbox - called from attack animation event
+    public void ActivateThustHitbox()
+    {
+        thrustHitbox.GetComponent<MeshCollider>().enabled = true;
+
+        if (showHitboxes)
+            thrustHitbox.GetComponent<MeshRenderer>().enabled = true;
+    }
+
+    //Activate the radial hitbox - called from attack animation event
+    public void ActivateRadialHitbox()
+    {
+        StartCoroutine(LerpRadialHitbox());
+    }
+
+    //Scales a circular wave of damage to hit enemies in a radius
+    private IEnumerator LerpRadialHitbox()
+    {
+        //Set starting position of damage radius to the impact point of the sword
+        radialHitbox.transform.position = swordImpactPoint.transform.position;
+
+        //Set up scaling variables
+        float attack3HitboxScale;
+        float hitboxOriginalScale = radialHitbox.transform.localScale.x;
+        float hitboxMinScale = hitboxOriginalScale / 100;
+        radialHitbox.transform.localScale = new Vector3(hitboxMinScale, 1, hitboxMinScale); //Shrink scale to min size
+
+        //Enable the hitbox
+        radialHitbox.GetComponent<SphereCollider>().enabled = true;
+        if (showHitboxes)
+            radialHitbox.GetComponent<MeshRenderer>().enabled = true;
+
+        //Lerp the damage wave to increase in scale over time
+        float timeElapsed = 0;
+        float duration = .25f;
+        while (timeElapsed < duration)
+        {
+            attack3HitboxScale = Mathf.Lerp(hitboxMinScale, hitboxOriginalScale, timeElapsed / duration);
+            radialHitbox.transform.localScale = new Vector3(attack3HitboxScale, 1, attack3HitboxScale);
+
+            timeElapsed += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        //disable the hitbox
+        radialHitbox.GetComponent<SphereCollider>().enabled = false;
+        if (showHitboxes)
+            radialHitbox.GetComponent<MeshRenderer>().enabled = false;
+
+        //reset the scale
+        radialHitbox.transform.localScale = new Vector3(hitboxOriginalScale, 1, hitboxOriginalScale); //reset scale
+    }
+
+    //Disable the slash hitbox - called from attack animation event
+    public void DeactivateSlashHitbox()
+    {
+        slashHitbox.GetComponent<MeshCollider>().enabled = false;
+
+        if (showHitboxes)
+            slashHitbox.GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    //Disable the thrust hitbox - called from attack animation event
+    public void DeactivateThrustHitbox()
+    {
+        thrustHitbox.GetComponent<MeshCollider>().enabled = false;
+
+        if (showHitboxes)
+            thrustHitbox.GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    public void DeactivateAllHitboxes()
+    {
+        DeactivateSlashHitbox();
+        DeactivateThrustHitbox();
+    }
+}
--- Assets/Scripts/Player/Sword.cs
+++ Assets/Scripts/Player/Sword.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
+    public bool dealsExtraDamage; //deals extra damage based on player's attack3 dmg mod if true
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<EnemyBase>())
         {
-            other.GetComponent<EnemyBase>().Damage(PlayerController.Instance.CurrAttackDamage);
+            //Determine dameage to deal based on player's current attack damage
+            float damageToDeal = PlayerController.Instance.CurrAttackDamage; ;
+
+            if (dealsExtraDamage)//Check to deal additional damage
+                damageToDeal *= PlayerController.Instance.attack3DmgModifier;
+
+            //Apply damage to enemy
+            other.GetComponent<EnemyBase>().Damage(damageToDeal);     
         }
     }
 }
