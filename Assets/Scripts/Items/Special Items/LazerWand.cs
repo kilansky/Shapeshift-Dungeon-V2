9683b3502a6ac61ae5a778914b1f6ca7e0db89e8
Alexander H. Lopez <alopez165@mail.csuchico.edu>
2021-04-13 16:07:30 -0700

    LazerWand is done!

--- Assets/Scripts/Items/Special Items/LazerWand.cs
+++ Assets/Scripts/Items/Special Items/LazerWand.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class LazerWand : MonoBehaviour
 {
     //Variable Initialization/Declaration
-    public GameObject PlayerLazerAttack; //Sets Lazer Wand ItemEquipment so we can get the prefab for the actual attack 
+    private GameObject PlayerLazerAttack; //Sets Lazer Wand ItemEquipment so we can get the prefab for the actual attack 
+    public List<GameObject> Lazers; //List to hold all the possible lazers to be spawned
 
     /// <summary>
     /// Function to get the Lazer Attack to spawn in the direction that the player is facing - AHL (4/13/21)
@@ -13,6 +14,8 @@ public class LazerWand : MonoBehaviour
     /// </summary>
     public void spawnLazer(Vector3 playerPos, Vector3 playerDir, Quaternion playerRotation)
     {
+        PlayerLazerAttack = Lazers[Random.Range(0, Lazers.Count)].gameObject;
+
         Vector3 spawnPos = playerPos + playerDir; //Sets the vector position to spawn the Lazer attack in front of the player
         spawnPos.y += 2f; //Adjusts the Lazer to spawn in the correct position to the player
 
