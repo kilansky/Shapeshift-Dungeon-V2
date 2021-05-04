58d62a148a9d929a04e57feb0df6301a57c4567a
JoeWarren95 <jlwarren@mail.csuchico.edu>
2021-05-03 19:14:21 -0700

    Setting up some base scripts for the skeleton, close to being at the same point as goblins, need to implement blocking next

---
+++ Assets/Scripts/Enemies/EnemySpecific/Skeleton/Skeleton_MoveState.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton_MoveState : MoveState
+{
+    private Skeleton enemy;
+
+    public Skeleton_MoveState(EnemyBase entity, FiniteStateMachine stateMachine, string animBoolName, D_MoveState stateData, Skeleton enemy) : base(entity, stateMachine, animBoolName, stateData)
+    {
+        this.enemy = enemy;
+    }
+
+    public override void DoChecks()
+    {
+        base.DoChecks();
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
+}
